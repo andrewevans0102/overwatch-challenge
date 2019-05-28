d4514c4a55d7a8fbce356d469f5734ee89c1db35
Andrew Evans <andrewevans0102@gmail.com>
2019-05-28 12:30:40 -0400

    file cleanup

--- frontend/src/app/static/home/home.component.ts
+++ frontend/src/app/static/home/home.component.ts
@@ -1,22 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { AngularFireAuth } from '@angular/fire/auth';
-import {FormControl, Validators} from '@angular/forms';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor(public router: Router) { }
-
-  ngOnInit() {
-
-  }
 
   createUser() {
     this.router.navigateByUrl('/create-user');
