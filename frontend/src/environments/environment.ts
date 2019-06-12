37f8914ea9393039c30e6e6b776acbbac0ed7bdf
Andrew Evans <andrewevans0102@gmail.com>
2019-06-12 09:19:57 -0400

    updating config for CICD deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,23 +4,23 @@
 
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
-  },
-  registrationCode: ''
-};
+    production: false,
+    firebase: {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    },
+    registrationCode: ''
+  };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
