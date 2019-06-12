b5c23964acc1bb303bd91022530738b04a6be6ff
Andrew Evans <45642358+andrewevans0102@users.noreply.github.com>
2019-06-12 01:31:56 -0400

    Create environment.prod.ts

---
+++ frontend/src/environment.prod.ts
@@ -0,0 +1,12 @@
+{
+    "baseUrl": "",
+    "env": {
+      "test-email": "",
+      "test-password": "",
+      "test-user-first-name": "",
+      "test-user-last-name": "",
+      "test-user-registration-code": ""
+    },
+    "projectId": ""
+  }
+  
