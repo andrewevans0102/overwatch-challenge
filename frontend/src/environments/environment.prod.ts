1ef11c59a7973dd057ed003129d006b753291357
Andrew Evans <andrewevans0102@gmail.com>
2019-06-12 00:52:18 -0400

    resolving issue with environment variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCQ-LP4qQec0LcWs7-Hubg9onf4R2S2VoY',
-    authDomain: 'overwatch-challenge.firebaseapp.com',
-    databaseURL: 'https://overwatch-challenge.firebaseio.com',
-    projectId: 'overwatch-challenge',
-    storageBucket: 'overwatch-challenge.appspot.com',
-    messagingSenderId: 'OC_messagingSenderId'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   },
-  registrationCode: 'skywalker_061419'
+  registrationCode: ''
 };
